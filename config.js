0406fa29deac771a2b6bfd4597a95c641fd1b4cd
Marcos Piriz <marcos.piriz@faverdelivery.com>
2023-12-20 10:16:54 -0300

    connection to mongodb in cloud

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 6001
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/clevendario'
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://marcos:slaketon@clevendario.banuefd.mongodb.net/?retryWrites=true&w=majority'
 export const JWT_SECRET = process.env.JWT_SECRET || 'clever-it'
 export const ACTIONS = [{id: 1, name: 'PRESENTIAL'}, {id:2, name: 'REMOTE'}]
 export const ROLES = [{id: 1, name: 'DEV'}, {id:2, name: 'ADMIN'}]
