f8cc8a97388cf6a0a9f33a47c15a81bbed963977
virginia-costa <costa.virginia7@gmail.com>
2023-12-21 12:18:48 -0300

    delete holidays

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,6 @@ const router = Router()
 import { welcome } from '../controllers/health.js'
 import { register, login } from '../controllers/user.js'
 import { create, getAllActions, getByEmail } from '../controllers/action.js'
-import { createHoliday, deleteHolidayById, editHolidayById, getAllHolidays } from '../controllers/feriados.js'
 
 router.get('/health', welcome)
 
@@ -22,18 +21,6 @@ router.get('/action/getAll', getAllActions)
 
 // action get by email
 router.get('/action/getByEmail', getByEmail)
-
-//action agregar feriado
-router.post('/newHoliday', createHoliday)
-
-//action ver feriados
-router.get('/holidays', getAllHolidays)
-
-//action editar feriado
-router.put('/holidays/edit/:id', editHolidayById)
-
-//action borrar feriado
-router.delete('/holidays/delete/:id', deleteHolidayById)
 
 
 export default router
